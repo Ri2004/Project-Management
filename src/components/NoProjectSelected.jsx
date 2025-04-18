1fdaf3fcdad9104f49f7932b519236e248d64cc9
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-04-18 17:22:55 +0530

    Alt text added

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -6,7 +6,7 @@ export default function NoProjectSelected({ onStartAddProject }) {
     <div className="mt-24 text-center w-2/3">
       <img
         src={noProjectImage}
-        alt=""
+        alt="an empty task list"
         className="w-16 h-16 object-contain mx-auto"
       />
       <h2 className="text-xl font-bold text-stone-500 my-4">
